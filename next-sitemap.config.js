fee324f17c31c94a39d480531184d16a2e062f27
mcgraneder <mcgrane480@gmail.com>
2023-02-28 15:21:01 +0000

    fix: sitemap

--- next-sitemap.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next-sitemap').IConfig} */
-const config = {
-  siteUrl: process.env.NEXT_PUBLIC_SITE_URL || "https://paalamugan.com",
-  generateRobotsTxt: true, // (optional)
-  // ...other options
-};
-
-module.exports = config;
