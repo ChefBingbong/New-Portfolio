5276b438500031d161cf650221a72c45bf4d7b56
Evan McGrane <evanmcgranebb98@gmail.com>
2024-11-25 14:09:15 +0000

    chore: remove crappy old projects

--- src/container/Work/data.ts
+++ src/container/Work/data.ts
@@ -66,24 +66,6 @@ export const worksData: ReadonlyArray<WorkData> = [
     tags: ["nextjs", "reactjs", "typescript"],
   },
   {
-    imgUrl: images.work.nextjsWebOAuth,
-    projectLink: "https://nextjs-web-oauth.vercel.app",
-    codeLink: "",
-    title: "Web OAuth",
-    description:
-      "Web OAuth Project developed using Next.js, NextAuth, Chakra UI, React, Redux, Redux Toolkit, TypeScript, Google OAuth, and more.",
-    tags: ["nextjs", "typescript"],
-  },
-  {
-    imgUrl: images.work.cryptocurrencyBitcoin,
-    projectLink: "https://cryptocurrency-bitcoin.netlify.app",
-    codeLink: "https://github.com/mcgraneder?tab=repositories",
-    title: "Crypto Bitcoin Price Chart",
-    description:
-      "Cryptocurrency Bitcoin Price Chart was developed using React, Typescript, coingecko-api, react-chartjs-2, chart.js and tailwindcss.",
-    tags: ["reactjs", "typescript", "tailwindcss"],
-  },
-  {
     imgUrl: images.work.backendCode,
     projectLink: "https://github.com/mcgraneder/AstralBridge-solV1/tree/master/contracts",
     codeLink: "https://github.com/mcgraneder/AstralBridge-solV1/tree/master/contracts",
@@ -101,33 +83,6 @@ export const worksData: ReadonlyArray<WorkData> = [
     description:
       "Custom build Arb bot with Node.js/Solididty which can arbiyrage DAI/WETH across Uniswap/Suishiswap and 2 other exchanges using flashloans",
     tags: ["nodejs", "typescript"],
-  },
-  {
-    imgUrl: images.work.translatorApplication,
-    projectLink: "https://translator-application.vercel.app",
-    codeLink: "",
-    title: "Translator Application",
-    description:
-      "It helps to translate from one language to multiple languages. It is developed using svelte, typescript, nodejs and more.",
-    tags: ["nodejs", "svelte", "typescript"],
-  },
-  {
-    imgUrl: images.work.backendCode,
-    projectLink: "https://github.com/mcgraneder/astral-sol",
-    codeLink: "https://github.com/mcgraneder/astral-sol",
-    title: "Crosschain AMM Contracts",
-    description:
-      "Solidity smart contracts for the Cross chain DEX i am currently working on. I use Renjs for bridging and intrface with the uniswap V3 contracts to preform swaps",
-    tags: ["solidity", "nodejs"],
-  },
-  {
-    imgUrl: images.work.backendCode,
-    projectLink: "https://github.com/mcgraneder/MoralisChallenges/tree/main/MORALIS",
-    codeLink: "https://github.com/mcgraneder/MoralisChallenges/tree/main/MORALIS",
-    title: "Gambling Contracts",
-    description:
-      "Solidity smart contracts for a gambling service that allows users to stake crypto assets in order to double their money. I use chainlink keepers and VRF for on chain randomness. In order to pay the chainlink fee i take a subsidy from the users bet to swap for LINK on uniswap to cover the fee",
-    tags: ["solidity", "nodejs"],
   },
   {
     imgUrl: images.work.backendCode,
